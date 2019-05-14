c56fc754f727f9cdce4232fec1f6f77bf3c1c1ab
dujianxiong <dujianxiong@sunlands.com>
2019-05-14 18:12:29 +0800

    嵌套侧边栏

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,15 +8,15 @@ module.exports = {
     }]
   ],
 
-  title: 'config title2',
-  description: 'config description2',
+  title: '格知',
+  description: '無善無惡心之體，有善有惡意之動，知善知惡是良知，為善去惡是格物。',
 
   themeConfig: {
     // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
-    repo: 'djxhero/know-my-life',
+    // repo: 'djxhero/know-my-life',
     // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
     // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
-    repoLabel: '查看源码',
+    // repoLabel: '查看源码',
 
     // 以下为可选的编辑链接选项
 
@@ -27,27 +27,63 @@ module.exports = {
     // 假如文档放在一个特定的分支下：
     // docsBranch: 'master',
     // 默认是 false, 设置为 true 来启用
-    editLinks: true,
+    // editLinks: true,
     // 默认为 "Edit this page"
-    editLinkText: '帮助我们改善此页面！',
+    // editLinkText: '帮助我们改善此页面！',
 
     displayAllHeaders: true,
     nav: [{
-      text: "指南",
-      link: "/guide/"
-    }],
-    sidebar: {
-      "/guide/": genGuideSidebar(true)
-
-    },
+      text: "gongkao",
+      items: [{
+          text: "yanyu",
+          link: "/gongkao/yanyu"
+        },
+        {
+          text: "changshi",
+          link: "/gongkao/changshi"
+        },
+        {
+          text: "shuliang",
+          link: "/gongkao/shuliang"
+        },
+        {
+          text: "panduan",
+          link: "/gongkao/panduan"
+        },
+        {
+          text: "ziliao",
+          link: "/gongkao/ziliao"
+        }
+      ]
+    }, ],
+    sidebar: [
+      {
+        title: 'Overview', // 必要的
+        path: '/gongkao/'// 可选的, 应该是一个绝对路径
+      },
+      {
+        title: 'yanyu', // 必要的
+        // path: '/gongkao/yanyu', // 可选的, 应该是一个绝对路径
+        collapsable: false, // 可选的, 默认值是 true,
+        sidebarDepth:2, // 可选的, 默认值是 1
+        children: [
+          'gongkao/yanyu/',
+          {
+            title: 'yanyu-xuanci', // 必要的
+            path: '/gongkao/yanyu/fill-word', // 可选的, 应该是一个绝对路径
+            collapsable: false // 可选的, 默认值是 true,
+          }
+        ]
+      },
+      {
+        title: 'changshi', // 必要的
+        collapsable: false, // 可选的, 默认值是 true,
+        sidebarDepth:2, // 可选的, 默认值是 1
+        children: [
+          'gongkao/changshi/'
+        ]
+      }
+    ],
     lastUpdated: 'Last Updated', // string | boolean
   }
-}
-
-function genGuideSidebar(isZh) {
-  return [{
-    title: isZh ? "快速入门" : "Getting Start",
-    collapsable: true,
-    children: ["", "quick-start", "install"]
-  }]
 }
